945be292fbb801f68fa42ac1d3be7d9688748ddc
Loïc Fauquet <fauquetloic@gmail.com>
2023-12-10 00:02:54 +0100

    Github Actions base

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  default: '/booru-video-checker/',
+  base: '/booru-video-checker/',
   plugins: [
     vue(),
     viteStaticCopy({
